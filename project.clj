41b8600a3ee7bfb2bf449b0a11cc4da444a84045
David Park <daviddpark@gmail.com>
2012-05-21 09:29:30 -0400

    opennlp deps

--- project.clj
+++ project.clj
@@ -5,9 +5,10 @@
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/data.json "0.1.2"]
                  [org.apache.opennlp/opennlp-tools "1.5.1-incubating"]
-                 [clojure-opennlp "0.1.11-SNAPSHOT"]
+                 [clojure-opennlp "0.1.11"]
                  [noir "1.2.1"]
+                 ;;[com.novemberain/monger "1.0.0-beta6"]
                 ]
   :profiles {:dev {:dependencies [[midje "1.3.1"]]}}
-  :jvm-opts ["-Xmx2048m"]
+  :jvm-opts ["-Xmx6g"]
   :main clj-lcoiapi.server)
